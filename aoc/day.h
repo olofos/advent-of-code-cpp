1f0a8e5f3b6db7cfab50eabd51bffd2975ff8fcb
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2024-12-03 17:04:23 +0100

    Better set of constructors for Day

--- aoc/day.h
+++ aoc/day.h
@@ -22,13 +22,19 @@ public:
     virtual std::string part2(std::istream& input) = 0;
     virtual int day() = 0;
     virtual int year() = 0;
-    virtual ~Day() {};
 
     virtual std::optional<std::string> part1_test_result() { return std::nullopt; };
     virtual std::optional<std::string> part2_test_result() { return std::nullopt; };
     virtual std::optional<std::string> test_input() { return std::nullopt; };
+
+    Day() = default;
+    virtual ~Day() = default;
+    Day(const Day&) = delete;
+    Day(Day&&) = default;
+    Day& operator=(const Day&) = delete;
+    Day& operator=(Day&&) = default;
 };
 
-bool run_all(std::vector<std::shared_ptr<Day>>& days);
+bool run_all(const std::vector<std::unique_ptr<Day>>& days);
 
 #endif
