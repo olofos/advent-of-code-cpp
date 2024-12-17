f11fa90d01b718d033c336e1232a52e4f8e37665
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2024-12-17 10:44:24 +0100

    Day 16 part 2

--- 2024/day16.cpp
+++ 2024/day16.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <iostream>
 #include <istream>
 #include <limits>
@@ -28,6 +29,26 @@ struct Node {
 
     auto operator<=>(const Node& other) const = default;
 };
+
+std::ostream& operator<<(std::ostream& os, const Node& node)
+{
+    char c;
+    switch (node.direction) {
+    case Dir::UP:
+        c = '^';
+        break;
+    case Dir::RIGHT:
+        c = '>';
+        break;
+    case Dir::DOWN:
+        c = 'v';
+        break;
+    case Dir::LEFT:
+        c = '<';
+        break;
+    }
+    return os << "(" << node.position.x << "," << node.position.y << "," << c << ")";
+}
 
 Dir turn_right(Dir dir)
 {
@@ -111,18 +132,25 @@ Grid parse_grid(std::istream& input)
     return grid;
 }
 
-std::string part1(std::istream& input)
+unsigned get_score(const std::map<Node, unsigned>& scores, Node node)
 {
-    auto grid = parse_grid(input);
-    auto start_pos = grid.find('S');
-    auto end = grid.find('E');
+    auto it = scores.find(node);
+    if (it == scores.end()) {
+        return std::numeric_limits<unsigned>::max();
+    }
+    return it->second;
+}
 
+std::pair<std::map<Node, unsigned>, std::map<Node, std::set<Node>>> dijkstra(const Grid& grid, Node start, Point end)
+{
     auto cmp = [](const std::pair<Node, unsigned>& lhs, const std::pair<Node, unsigned>& rhs) { return lhs.second > rhs.second; };
     std::priority_queue<std::pair<Node, unsigned>, std::vector<std::pair<Node, unsigned>>, decltype(cmp)> queue(cmp);
 
-    queue.push({ { start_pos, Dir::RIGHT }, 0 });
+    queue.push({ start, 0 });
 
     std::set<Node> visited;
+    std::map<Node, unsigned> scores = { { start, 0 } };
+    std::map<Node, std::set<Node>> prev;
 
     while (!queue.empty()) {
         auto [node, score] = queue.top();
@@ -131,26 +159,91 @@ std::string part1(std::istream& input)
             continue;
         }
         visited.insert(node);
-        if (node.position == end) {
-            return std::to_string(score);
-        }
+
         std::pair<Node, unsigned> neighbors[3] = {
             { { node.position + node.direction, node.direction }, score + 1 },
             { { node.position + turn_left(node.direction), turn_left(node.direction) }, score + 1001 },
             { { node.position + turn_right(node.direction), turn_right(node.direction) }, score + 1001 },
         };
         for (auto& neighbor : neighbors) {
-            if (grid(neighbor.first.position) != '#') {
-                queue.push(neighbor);
+            if (grid(neighbor.first.position) == '#') {
+                continue;
             }
+            if (get_score(scores, neighbor.first) > neighbor.second) {
+                scores[neighbor.first] = neighbor.second;
+                prev[neighbor.first] = { node };
+            } else if (get_score(scores, neighbor.first) == neighbor.second) {
+                prev[neighbor.first].insert(node);
+            }
+            queue.push(neighbor);
         }
     }
-    throw std::runtime_error("no path found");
+
+    return { scores, prev };
+}
+
+std::string part1(std::istream& input)
+{
+    auto grid = parse_grid(input);
+    auto start = grid.find('S');
+    auto end = grid.find('E');
+
+    auto [scores, prev] = dijkstra(grid, { start, Dir::RIGHT }, end);
+
+    std::array<Node, 4> end_nodes = {
+        { { end, Dir::RIGHT }, { end, Dir::UP }, { end, Dir::DOWN }, { end, Dir::LEFT } },
+    };
+
+    if (std::ranges::all_of(end_nodes, [&prev](const Node& node) { return prev.find(node) != prev.end(); })) {
+        throw std::runtime_error("no path found");
+    }
+
+    std::array<unsigned, 4> end_scores;
+    std::ranges::transform(end_nodes, end_scores.begin(), [&](const Node& node) { return get_score(scores, node); });
+    auto min_score = std::ranges::min(end_scores);
+
+    return std::to_string(min_score);
 }
 
 std::string part2(std::istream& input)
 {
-    throw std::runtime_error("not implemented");
+
+    auto grid = parse_grid(input);
+    auto start = grid.find('S');
+    auto end = grid.find('E');
+
+    auto [scores, prev] = dijkstra(grid, { start, Dir::RIGHT }, end);
+
+    std::array<Node, 4> end_nodes = {
+        { { end, Dir::RIGHT }, { end, Dir::UP }, { end, Dir::DOWN }, { end, Dir::LEFT } },
+    };
+
+    if (std::ranges::all_of(end_nodes, [&prev](const Node& node) { return prev.find(node) != prev.end(); })) {
+        throw std::runtime_error("no path found");
+    }
+
+    std::array<unsigned, 4> end_scores;
+    std::ranges::transform(end_nodes, end_scores.begin(), [&](const Node& node) { return get_score(scores, node); });
+    auto min_score = std::ranges::min(end_scores);
+
+    std::set<Point> all_points;
+    for (auto end : end_nodes) {
+        if (get_score(scores, end) != min_score) {
+            continue;
+        }
+        std::set<Node> curr_nodes = { end };
+
+        while (!curr_nodes.empty()) {
+            std::set<Node> prev_nodes;
+            for (auto node : curr_nodes) {
+                prev_nodes.insert(prev[node].begin(), prev[node].end());
+                all_points.insert(node.position);
+            }
+            curr_nodes.swap(prev_nodes);
+        }
+    }
+
+    return std::to_string(all_points.size());
 }
 }
 
@@ -160,7 +253,7 @@ DayDescription Day16::description()
     description.part1 = day16::part1;
     description.part2 = day16::part2;
     description.part1_test_result = "7036";
-    // description.part2_test_result = "";
+    description.part2_test_result = "45";
     description.test_input = "###############\n#.......#....E#\n#.#.###.#.###.#\n#.....#.#...#.#\n#.###.#####.#.#\n#.#.#.......#.#\n#.#.#####.###.#\n#...........#.#\n###.#.#####.#.#\n#...#.....#.#.#\n#.#.#.###.#.#.#\n#.....#...#.#.#\n#.###.#.#.#.#.#\n#S..#.....#...#\n###############";
     return description;
 }
