050ae8d04356390a74a44d20f822ecdd4c90b32e
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2024-12-22 22:50:26 +0100

    Day 12 part 2

--- 2024/day12.cpp
+++ 2024/day12.cpp
@@ -61,13 +61,42 @@ std::string part1(std::istream& input)
             }
         }
     }
-
     return std::to_string(score);
 }
 
 std::string part2(std::istream& input)
 {
-    throw std::runtime_error("not implemented");
+    std::vector<std::string> lines = aoc::read_lines_with_border(input);
+
+    uint64_t score = 0;
+    for (unsigned y = 1; y < lines.size() - 1; y++) {
+        for (unsigned x = 1; x < lines[y].size() - 1; x++) {
+            if (lines[y][x] != '.') {
+                auto region = flood_fill(lines, { x, y });
+                unsigned corners = 0;
+                for (auto& p : region.points) {
+                    bool d0 = region.points.contains({ p.x - 1, p.y - 1 });
+                    bool d1 = region.points.contains({ p.x, p.y - 1 });
+                    bool d2 = region.points.contains({ p.x + 1, p.y - 1 });
+                    bool d3 = region.points.contains({ p.x - 1, p.y });
+                    bool d4 = region.points.contains({ p.x + 1, p.y });
+                    bool d5 = region.points.contains({ p.x - 1, p.y + 1 });
+                    bool d6 = region.points.contains({ p.x, p.y + 1 });
+                    bool d7 = region.points.contains({ p.x + 1, p.y + 1 });
+
+                    bool corner_nw = (!(d1 || d3) || (d1 && d3 && !d0)) ? 1 : 0;
+                    bool corner_sw = (!(d3 || d6) || (d3 && d6 && !d5)) ? 1 : 0;
+                    bool corner_se = (!(d6 || d4) || (d6 && d4 && !d7)) ? 1 : 0;
+                    bool corner_ne = (!(d4 || d1) || (d4 && d1 && !d2)) ? 1 : 0;
+
+                    corners += corner_nw + corner_sw + corner_se + corner_ne;
+                }
+                score += corners * region.area;
+            }
+        }
+    }
+
+    return std::to_string(score);
 }
 }
 
@@ -77,7 +106,7 @@ DayDescription Day12::description()
     description.part1 = day12::part1;
     description.part2 = day12::part2;
     description.part1_test_result = "1930";
-    // description.part2_test_result = "";
+    description.part2_test_result = "1206";
     description.test_input = "RRRRIICCFF\nRRRRIICCCF\nVVRRRCCFFF\nVVRCCCJFFF\nVVVVCJJCFE\nVVIVCCJJEE\nVVIIICJJEE\nMIIIIIJJEE\nMIIISIJEEE\nMMMISSJEEE";
     return description;
 }
