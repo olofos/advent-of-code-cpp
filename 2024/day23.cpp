c373f937849fdbed1c95b8d10a1d388e944fb6bc
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2024-12-24 10:30:05 +0100

    Day 23 part 2 using external library

--- 2024/day23.cpp
+++ 2024/day23.cpp
@@ -9,23 +9,25 @@
 #include <vector>
 
 #include "days.h"
+#include "mcqd.h"
 
 namespace day23 {
 std::string part1(std::istream& input)
 {
     std::string line;
-    std::vector<std::string> computers;
+    std::set<std::string> computer_set;
     std::set<std::pair<std::string, std::string>> links;
-    std::map<std::string, std::set<std::string>> links_from;
+    std::map<std::string, std::set<std::string>> neighbors;
     while (std::getline(input, line)) {
         auto a = line.substr(0, 2);
         auto b = line.substr(3);
         links.insert(std::minmax(a, b));
-        computers.push_back(a);
-        computers.push_back(b);
-        links_from[a].insert(b);
-        links_from[b].insert(a);
+        computer_set.insert(a);
+        computer_set.insert(b);
+        neighbors[a].insert(b);
+        neighbors[b].insert(a);
     }
+    std::vector<std::string> computers(computer_set.begin(), computer_set.end());
     std::partition(computers.begin(), computers.end(), [](auto& c) { return c[0] == 't'; });
 
     std::set<std::array<std::string, 3>> paths;
@@ -34,9 +36,9 @@ std::string part1(std::istream& input)
         if (a[0] != 't') {
             break;
         }
-        for (auto it_b = links_from[a].begin(); it_b != links_from[a].end(); ++it_b) {
+        for (auto it_b = neighbors[a].begin(); it_b != neighbors[a].end(); ++it_b) {
             auto& b = *it_b;
-            for (auto it_c = std::next(it_b); it_c != links_from[a].end(); ++it_c) {
+            for (auto it_c = std::next(it_b); it_c != neighbors[a].end(); ++it_c) {
                 auto& c = *it_c;
                 if (links.find(std::minmax(b, c)) != links.end()) {
                     std::array<std::string, 3> cs = { a, b, c };
@@ -52,7 +54,46 @@ std::string part1(std::istream& input)
 
 std::string part2(std::istream& input)
 {
-    throw std::runtime_error("not implemented");
+    std::string line;
+    std::set<std::string> computer_set;
+    std::map<std::string, std::set<std::string>> neighbors;
+    while (std::getline(input, line)) {
+        auto a = line.substr(0, 2);
+        auto b = line.substr(3);
+        computer_set.insert(a);
+        computer_set.insert(b);
+        neighbors[a].insert(b);
+        neighbors[b].insert(a);
+    }
+
+    std::vector<std::string> computers(computer_set.begin(), computer_set.end());
+
+    bool** conn = new bool*[computers.size()];
+    for (size_t i = 0; i < computers.size(); i++) {
+        conn[i] = new bool[computers.size()];
+        for (size_t j = 0; j < computers.size(); j++) {
+            conn[i][j] = neighbors[computers[i]].contains(computers[j]);
+        }
+    }
+
+    Maxclique m(conn, static_cast<int>(computers.size()));
+    int* qmax;
+    int qmaxsize = 0;
+    m.mcq(qmax, qmaxsize);
+
+    std::set<std::string> qmaxset;
+    for (int i = 0; i < qmaxsize; i++) {
+        qmaxset.insert(computers[qmax[i]]);
+    }
+
+    std::string result;
+    for (auto& s : qmaxset) {
+        result += s;
+        result += ",";
+    }
+    result.pop_back();
+
+    return result;
 }
 }
 
@@ -62,7 +103,7 @@ DayDescription Day23::description()
     description.part1 = day23::part1;
     description.part2 = day23::part2;
     description.part1_test_result = "7";
-    // description.part2_test_result = "";
+    description.part2_test_result = "co,de,ka,ta";
     description.test_input = "kh-tc\nqp-kh\nde-cg\nka-co\nyn-aq\nqp-ub\ncg-tb\nvc-aq\ntb-ka\nwh-tc\nyn-cg\nkh-ub\nta-co\nde-co\ntc-td\ntb-wq\nwh-td\nta-ka\ntd-qp\naq-cg\nwq-ub\nub-vc\nde-ta\nwq-aq\nwq-vc\nwh-yn\nka-de\nkh-ta\nco-tc\nwh-qp\ntb-vc\ntd-yn";
     return description;
 }
